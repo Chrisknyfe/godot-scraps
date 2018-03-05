bd352ca7d466cf70200073c268fc79457e387f5a
Zach Bernal <chrisknyfe@gmail.com>
2018-03-04 19:32:29 -0800

    ocean shader refraction, albedo flow, albedo masking

--- water_shader/MeshGen.gd
+++ water_shader/MeshGen.gd
@@ -1,8 +1,9 @@
-
+tool 
 extends MeshInstance
 
 export(Material) 	var mat
 export(float) 		var scaleFactor = 0.10
+export(float)		var matScaleFactor = 1.0
 
 # Due to the "tool" keyword at the top of this file
 # this function already executes in the editor
@@ -19,19 +20,19 @@ func _ready():
 			
 			var f_width = float(width)
 			
-			var uv_ul = Vector2( x/f_width + 0.5, z/f_width + 0.5 )
+			var uv_ul = Vector2( x/f_width + 0.5, z/f_width + 0.5 ) * matScaleFactor
 			var vert_ul = Vector3(x, 0, z) * scaleFactor
 			
-			var uv_ur = Vector2( (x+1)/f_width + 0.5, z/f_width + 0.5 )
+			var uv_ur = Vector2( (x+1)/f_width + 0.5, z/f_width + 0.5 ) * matScaleFactor
 			var vert_ur = Vector3(x+1, 0, z) * scaleFactor
 			
-			var uv_lr = Vector2( (x+1)/f_width + 0.5, (z+1)/f_width + 0.5 )
+			var uv_lr = Vector2( (x+1)/f_width + 0.5, (z+1)/f_width + 0.5 ) * matScaleFactor
 			var vert_lr = Vector3(x+1, 0, z+1) * scaleFactor
 			
-			var uv_ll = Vector2( x/f_width + 0.5, (z+1)/f_width + 0.5 )
+			var uv_ll = Vector2( x/f_width + 0.5, (z+1)/f_width + 0.5 ) * matScaleFactor
 			var vert_ll = Vector3(x, 0, z+1) * scaleFactor
 			
-			print("meshp %d,%d: " % [x,z], vert_ul, uv_ul)
+#			print("meshp %d,%d: " % [x,z], vert_ul, uv_ul)
 			
 			surfTool.add_uv(uv_ul)
 			surfTool.add_vertex(vert_ul)
