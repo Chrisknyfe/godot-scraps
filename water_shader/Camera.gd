c85e587d3e964bcf3dea4dd29006ce00276767a6
Zach Bernal <chrisknyfe@gmail.com>
2018-03-13 02:57:38 -0700

    Smoother camera for video capture

--- water_shader/Camera.gd
+++ water_shader/Camera.gd
@@ -9,6 +9,8 @@ onready var speed = 1
 onready var velocity = Vector3()
 onready var initial_rotation = PI/2
 
+var rot_target = Vector3(0,0,0)
+
 func _enter_tree():
 	# Capture the mouse (can be toggled by pressing F10)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -17,14 +19,30 @@ func _ready():
 #	OS.set_window_maximized(true)
 	OS.set_window_fullscreen(true)
 
+func slerp_euler_no_z(initial_rot, target_rot, weight):
+	var init_trans = Transform(Basis(initial_rot), Vector3(0,0,0))
+	var target_trans = Transform(Basis(target_rot), Vector3(0,0,0))
+	
+	var final_trans = init_trans.interpolate_with(target_trans, weight)
+	var final_rot = final_trans.basis.get_euler()
+	final_rot.z = 0
+	return final_rot
+	
 func _input(event):
 	# Horizontal mouse look
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotation.y -= event.relative.x*MOUSE_SENSITIVITY
+		rot_target.y -= event.relative.x*MOUSE_SENSITIVITY
+		
+		# wrap horizontal mouselook
+		if rot_target.y > PI:
+			rot_target.y -= 2*PI
+		if rot_target.y < -PI:
+			rot_target.y += 2*PI
 
 	# Vertical mouse look, clamped to -90..90 degrees
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotation.x = clamp(rotation.x - event.relative.y*MOUSE_SENSITIVITY, deg2rad(-90), deg2rad(90))
+		rot_target.x = clamp(rot_target.x - event.relative.y*MOUSE_SENSITIVITY, deg2rad(-90), deg2rad(90))
+		
 
 	# Toggle HUD
 	if event.is_action_pressed("toggle_hud"):
@@ -50,6 +68,9 @@ func _input(event):
 		
 
 func _physics_process(delta):
+	# Apply rotation
+	rotation = slerp_euler_no_z(rotation, rot_target, 20 * delta)
+	
 	# Speed modifier
 	if Input.is_action_pressed("move_speed"):
 		speed = 4
