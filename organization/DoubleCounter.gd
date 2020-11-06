ad61eb33924051fc85ac20a1c7dfa14a33b2f18d
Chrisknyfe <chrisknyfe@gmail.com>
2020-11-05 21:56:54 -0800

    Inherited custom classes yesssss

---
+++ organization/DoubleCounter.gd
@@ -0,0 +1,8 @@
+extends CustomCounter
+
+class_name DoubleCounter
+
+# This is a custom class that inherits from another custom class. Nice.
+
+func increment():
+	self.count += 2.0
--- organization/organization_demo.gd
+++ organization/organization_demo.gd
@@ -7,15 +7,18 @@ onready var abox_scene = preload("res://abox.tscn")
 
 # Instancing a custom non-node class
 var counter = CustomCounter.new()
+var anglecounter = DoubleCounter.new()
 
 func makebox():
 	# Instancing a packed scene (or a subscene, whatever you wanna call it)
 	var newbox = abox_scene.instance()
-	var c : float = counter.get_count()
-	newbox.translate(Vector3(0,float(c)/5.0,0))
-	newbox.rotate(Vector3(0,1,0), float(c)/5.0)
+	var y : float = counter.get_count() / 5.0
+	var theta : float = anglecounter.get_count() / 5.0
+	newbox.translate(Vector3(0,y,0))
+	newbox.rotate(Vector3(0,1,0), theta)
 	self.add_child(newbox)
 	counter.increment()
+	anglecounter.increment()
 
 func _process(delta):
 	if Input.is_action_just_pressed("makebox"):
