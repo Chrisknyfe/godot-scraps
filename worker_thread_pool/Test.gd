84ab94ee252b56169798f066f1d681ba8dc3d613
Chrisknyfe <chrisknyfe@gmail.com>
2021-10-28 00:13:12 -0700

    removed timers; reached enlightenment

--- worker_thread_pool/Test.gd
+++ worker_thread_pool/Test.gd
@@ -3,30 +3,29 @@ extends Node
 var module_lock = Mutex.new()
 var completed_tasks = 0
 
+signal reached_enlightenment
+
 func _ready():
 	test()
 
 func test():
+	# You can add more workers at runtime! Just in case.
 	WorkerThreadPool.start_workers(2)
-	yield(get_tree().create_timer(1.0), "timeout")
 
-	# Add work
 	WorkerThreadPool.add_work(self, "fake_work")
 	WorkerThreadPool.add_work(self, "fake_work", "fake userdata")
 	WorkerThreadPool.add_work(self, "fake_work", "more fake asdfas", "fake_work_done")
 	
-	print("Complete the 108 tasks to achieve enlightenment")
-	for i in range(108):
-		WorkerThreadPool.add_work(self, "silent_buddhist_task", "om", "test_increment_completed_tasks")
-
-	yield(get_tree().create_timer(5.0), "timeout")
+	var number_of_tasks = 108
+	print("Complete the %d tasks to achieve enlightenment" % number_of_tasks)
+	for i in range(number_of_tasks):
+		WorkerThreadPool.add_work(self, "silent_buddhist_task", number_of_tasks, "test_increment_completed_tasks")
+	
+	# Ironic that this would be the place I actually learn how to use signals.
+	yield(self, "reached_enlightenment")
+	
 	print("Completed %d tasks" % completed_tasks)
-	assert(completed_tasks == 108)
-
-	# Add work that causes a timeout
-	WorkerThreadPool.add_work(self, "long_fake_work", "more fake asdfas", "fake_work_done", 0.1)
-	WorkerThreadPool.add_work(self, "long_fake_work", "yare yare daze.", "fake_work_done", 0.1, "test_custom_timeout")
-	yield(get_tree().create_timer(2.0), "timeout")
+	assert(completed_tasks == number_of_tasks)
 	
 	print("Shutting down all workers!")
 	WorkerThreadPool.stop_workers()
@@ -37,23 +36,19 @@ func test():
 	
 func fake_work(userdata):
 	print("fake work userdata: ", userdata)
-	return "fake work results"
-	
-func silent_buddhist_task(userdata):
-	return userdata
-	
-func test_increment_completed_tasks(userdata):
-	module_lock.lock()
-	completed_tasks += 1
-	module_lock.unlock()
-
-func long_fake_work(userdata):
-	print("fake work userdata: ", userdata)
 	OS.delay_msec(500)
 	return "fake work results"
 	
-func test_custom_timeout(userdata):
-	print("My task timed out! ", userdata)
-	
 func fake_work_done(userdata):
 	print("fake work done callback: ", userdata)
+	
+func silent_buddhist_task(number_of_tasks):
+	return number_of_tasks
+	
+func test_increment_completed_tasks(number_of_tasks):
+	module_lock.lock()
+	completed_tasks += 1
+	if completed_tasks >= number_of_tasks:
+		self.emit_signal("reached_enlightenment")
+	module_lock.unlock()
+	
--- worker_thread_pool/WorkerThreadPool.gd
+++ worker_thread_pool/WorkerThreadPool.gd
@@ -1,20 +1,16 @@
 extends Node
 
 export var default_worker_thread_count = 10
-export var default_timeout: float = 10.0
 
 class ThreadInfo:
 	var worker_id: int
 	var thread: Thread
-	var timer: Timer
 	
 class Task:
 	var instance: Object
 	var method: String
 	var userdata = null
 	var done_callback: String = ""
-	var timeout: float
-	var timeout_callback: String = ""
 
 # module data lock
 var module_lock = Mutex.new()
@@ -22,7 +18,6 @@ var module_lock = Mutex.new()
 # work queue
 var work_ready = Semaphore.new()
 var work_queue = [] # contains Task objects
-var completed_task_count = 0 # debug odometer
 
 # worker thread management
 var next_worker_id = 1
@@ -51,8 +46,6 @@ func start_workers(num_workers: int = default_worker_thread_count):
 		info.thread = thread
 		var error = thread.start(self, "_worker_thread", info)
 		assert(error == OK)
-		info.timer = Timer.new()
-		add_child(info.timer)
 		
 		lock()
 		threadpool[worker_id] = info
@@ -87,36 +80,23 @@ func stop_workers():
 	
 	lock()
 	for worker_id in worker_id_list:
-		var timer = threadpool[worker_id].timer
-		remove_child(timer)
-		timer.queue_free()
 		threadpool.erase(worker_id)
 	unlock()
 		
-func add_work(instance: Object, method: String, userdata = null, done_callback: String = "", timeout: float = default_timeout, timeout_callback: String = ""):
+func add_work(instance: Object, method: String, userdata = null, done_callback: String = ""):
 	var task = Task.new()
 	task.instance = instance
 	task.method = method
 	task.userdata = userdata
 	task.done_callback = done_callback
-	task.timeout = timeout
-	task.timeout_callback = timeout_callback
 	lock()
 	work_queue.push_front(task)
 	unlock()
 	var error = work_ready.post()
 	assert(error == OK)
 	
-func get_num_tasks():
-	lock()
-	var num = work_queue.size()
-	unlock()
-	return num
-	
 func _worker_thread(thread_info: ThreadInfo):
 	var worker_id = thread_info.worker_id
-	var timer = thread_info.timer
-	timer.one_shot = true
 	var running = true
 	while (running):
 		work_ready.wait()
@@ -133,25 +113,6 @@ func _worker_thread(thread_info: ThreadInfo):
 		unlock()
 		
 		if task:
-			var timeout_instance = self
-			var timeout_callback = "_on_task_timeout"
-			var timeout_args = [task, worker_id]
-			if task.timeout_callback:
-				timeout_instance = task.instance
-				timeout_callback = task.timeout_callback
-				timeout_args = [task.userdata]
-				
-			timer.connect("timeout", timeout_instance, timeout_callback, timeout_args)
-			timer.start(task.timeout)
 			var results = task.instance.call(task.method, task.userdata)
 			if task.done_callback:
 				task.instance.call_deferred(task.done_callback, results)
-			timer.stop()
-			timer.disconnect("timeout", timeout_instance, timeout_callback)
-			
-			lock()
-			completed_task_count += 1
-			unlock()
-	
-func _on_task_timeout(task: Task, worker_id: int):
-	print("Worker %d: Task %s timed out after %f sec!" % [worker_id, str(task.instance) + "." + task.method, task.timeout])
